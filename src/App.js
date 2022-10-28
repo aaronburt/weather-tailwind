7333c00d1ffe0c92e8d67884f29fe219cb6c287f
aaronburt <email@aaronburt.co.uk>
2022-10-28 12:56:31 +0100

    Reduced 7 days down to 5

--- src/App.js
+++ src/App.js
@@ -10,20 +10,17 @@ export default function App(){
             try {
                 const response = await fetch('https://weather.streamsave.xyz/get?city=congleton');
                 const data = await response.json();
-
                 const daily = data.daily;
                 const baseIconUrl = "https://openweathermap.org/img/wn/";
 
-
                 const minmax = Object.keys(daily).map(([k, v]) => { 
-
-                let day = daily[k];
-                let days = ["Sunday", "Monday", "Tuesday", "Wednesday","Thursday", "Friday","Saturday"]
-                
+                    if(k >= 5) return;
+                    let day = daily[k];
+                    let days = ["Sunday", "Monday", "Tuesday", "Wednesday","Thursday", "Friday","Saturday"];
                     return (
                         <div key={k} className="text-center text-black divide-x-2 hover:bg-slate-100">
                             <div className="font-light text-2xl">{days[new Date(day.dt * 1000).getDay()]}</div>
-                            <div class>{Math.ceil(day.temp.max)}°c</div>
+                            <div>{Math.ceil(day.temp.max)}°c</div>
                             <div>{Math.floor(day.temp.max)}°c</div>
                             <div>{day.weather[0].main}</div>
                             <div className="justify-center">
@@ -33,7 +30,6 @@ export default function App(){
                     );
                 });
             
-
                 setWeather(minmax);
             } catch(err){
                 return;
@@ -48,7 +44,7 @@ export default function App(){
 
     return(
         <div className="h-screen w-full flex">
-            <div className="m-auto w-11/12 grid grid-cols-8 p-4 bg-white rounded-xl">
+            <div className="m-auto w-11/12 grid grid-cols-5 p-4 bg-white rounded-xl">
                 {weather}
             </div>
         </div>
