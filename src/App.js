b780b07296a793703995645179120409042a09d8
aaronburt <email@aaronburt.co.uk>
2022-10-28 12:46:47 +0100

    Starting...

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App(){
+
+    const [weather, setWeather] = useState('');
+
+    useEffect(()=>{
+
+        async function getWeather(){
+            try {
+                const response = await fetch('https://weather.streamsave.xyz/get?city=congleton');
+                const data = await response.json();
+
+                const daily = data.daily;
+                const baseIconUrl = "https://openweathermap.org/img/wn/";
+
+
+                const minmax = Object.keys(daily).map(([k, v]) => { 
+
+                let day = daily[k];
+                let days = ["Sunday", "Monday", "Tuesday", "Wednesday","Thursday", "Friday","Saturday"]
+
+
+                    return (
+                        <div key={k} className="text-center text-black divide-x-2 hover:bg-slate-100">
+                            <div className="font-light text-2xl">{days[new Date(day.dt * 1000).getDay()]}</div>
+                            <div class>{Math.ceil(day.temp.max)}°c</div>
+                            <div>{Math.floor(day.temp.max)}°c</div>
+                            <div>{day.weather[0].main}</div>
+                            <div className="justify-center">
+                                <img className="mx-auto" src={baseIconUrl + day.weather[0].icon + '@2x.png'}/>
+                            </div>
+                        </div>
+                    );
+                });
+            
+
+                setWeather(minmax);
+            } catch(err){
+                return;
+            }
+        }
+
+        getWeather();
+
+
+    }, []);
+
+
+    return(
+        <div className="h-screen w-full flex">
+            <div className="m-auto w-11/12 grid grid-cols-8 p-4 bg-white rounded-xl">
+                {weather}
+            </div>
+        </div>
+    )
+
+
+
+
 }
-
-export default App;
